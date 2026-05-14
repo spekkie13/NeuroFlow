df4eb4a5cf1c88981a58345c87c64428efac569f
spekkie13 <32901490+spekkie13@users.noreply.github.com>
2026-05-14 23:31:49 +0200

    added stuff

---
+++ apps/packages/api/src/repositories/taskRepository.ts
@@ -0,0 +1,56 @@
+﻿import {Task, TaskInsert, TaskUpdate} from "../types/db.types";
+import { db } from '../db/index.js'
+import {tasks} from "../db/schema";
+import {and, eq} from "drizzle-orm";
+
+export class TaskRepository {
+    async getTasksByProject(userId: string, projectId: string): Promise<Task[]> {
+        return await db
+            .select()
+            .from(tasks)
+            .where(
+                and(
+                    eq(tasks.userId, userId),
+                    eq(tasks.projectId, projectId)
+                )
+            )
+    }
+
+    async createTask(task: TaskInsert): Promise<Task> {
+        const [result] = await db.insert(tasks)
+            .values(task)
+            .onConflictDoUpdate({
+                target: tasks.id,
+                set: { name: task.name, completed: task.completed, priority: task.priority, date: task.date, notes: task.notes, estimatedMinutes: task.estimatedMinutes, updatedAt: new Date() }
+            })
+            .returning();
+
+        return result;
+    }
+
+    async updateTask(userId: string, task: TaskUpdate): Promise<Task> {
+        const [updatedTask] = await db
+            .update(tasks)
+            .set({ userId: task.userId, name: task.name, completed: task.completed, priority: task.priority, date: task.date, notes: task.notes, estimatedMinutes: task.estimatedMinutes, updatedAt: new Date() })
+            .where(and(
+                eq(tasks.userId, userId),
+                eq(tasks.id, task.id))
+            )
+            .returning();
+
+        return updatedTask;
+    }
+
+    async deleteTask(userId: string, taskId: string): Promise<void> {
+        await db
+            .delete(tasks)
+            .where(
+                and(
+                    eq(tasks.userId, userId),
+                    eq(tasks.id, taskId)
+                )
+            )
+    }
+}
+
+export const taskRepository = new TaskRepository();
